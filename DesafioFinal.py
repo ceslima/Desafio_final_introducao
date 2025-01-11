4027bc8c2fca0d4288bc940fb0b4838355a3f40a
Carlos Lima <caddu.lima@hotmail.com>
2025-01-11 11:40:29 -0300

    DesafioFinal.py
    Código do desafio

--- DesafioFinal.py
+++ DesafioFinal.py
@@ -1,1 +1,30 @@
+class Heroi():
+    def __init__(self, nome, idade, tipo):
+        self.nome = nome
+        self.idade = idade
+        self.tipo = tipo
+    def atacar(self):
+        ataque = ""
 
+        if self.tipo == 'mago':
+            ataque = 'magia'
+        elif self.tipo == 'guerreiro':
+            ataque = 'espada'
+        elif self.tipo == 'monge':
+            ataque = 'artes marciais'
+        elif self.tipo == 'ninja':
+            ataque = 'shuriken'
+        else:
+            ataque = 'um ataque desconhecido'
+
+        print(f"O {self.tipo} de nome {self.nome} atacou usando {ataque}\n")
+
+#exemplo
+heroi1 = Heroi('Garem',30, 'guerreiro')
+heroi2 = Heroi('Ryze', 150, 'mago')
+heroi3 = Heroi('Lee', 70, 'monge')
+heroi4 = Heroi('Xin', 70, 'ninja')
+heroi1.atacar()
+heroi2.atacar()
+heroi3.atacar()
+heroi4.atacar()
